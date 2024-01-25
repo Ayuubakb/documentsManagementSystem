1bed74799b81d619e0d0cc4b6d177402f95d651a
Akoubri Ayoub <123371824+Ayuubakb@users.noreply.github.com>
2024-01-25 21:06:04 +0100

    Update sendMail.php

--- php/sendMail.php
+++ php/sendMail.php
@@ -152,8 +152,8 @@ if(!strcmp($q,"demanderel")){
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'akoubriayoub@gmail.com';                     //SMTP username
-    $mail->Password   = 'cxbwlbjbdeuacarn';                               //SMTP password
+    $mail->Username   = '*****@gmail.com';                     //SMTP username
+    $mail->Password   = '********';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
